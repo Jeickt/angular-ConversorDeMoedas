ad2020222f2e62b205b35950fec1d340af242fd2
Jeison Leandro Rückert <jeison.let@gmail.com>
2022-01-30 16:49:55 -0300

    Implementando modal de conversão de moedas

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,10 +1,12 @@
 import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { ConversorModule } from './conversor/conversor.module';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [AppComponent],
+      imports: [ConversorModule],
     }).compileComponents();
   });
 
--- src/app/conversor/conversor.module.ts
+++ src/app/conversor/conversor.module.ts
@@ -6,9 +6,11 @@ import { FormsModule } from '@angular/forms';
 import { ConversorComponent } from './components/conversor.component';
 import { MoedaService, ConversorService } from './services';
 import { NumeroDirective } from './directives/numero.directive';
+import { ModalCotacaoComponent } from './utils/modal-cotacao.component';
+import { DataBrPipe } from './pipes/data-br.pipe';
 
 @NgModule({
-  declarations: [ConversorComponent, NumeroDirective],
+  declarations: [ConversorComponent, NumeroDirective, ModalCotacaoComponent, DataBrPipe],
   imports: [CommonModule, HttpClientModule, FormsModule],
   exports: [ConversorComponent],
   providers: [MoedaService, ConversorService],
