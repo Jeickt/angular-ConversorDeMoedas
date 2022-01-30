2021929e873f30b2519f475ab7a91f55fb421ed6
Jeison Leandro Rückert <jeison.let@gmail.com>
2022-01-30 15:45:11 -0300

    Implementando diretiva de validação de números

--- src/app/conversor/conversor.module.ts
+++ src/app/conversor/conversor.module.ts
@@ -5,9 +5,10 @@ import { FormsModule } from '@angular/forms';
 
 import { ConversorComponent } from './components/conversor.component';
 import { MoedaService, ConversorService } from './services';
+import { NumeroDirective } from './directives/numero.directive';
 
 @NgModule({
-  declarations: [ConversorComponent],
+  declarations: [ConversorComponent, NumeroDirective],
   imports: [CommonModule, HttpClientModule, FormsModule],
   exports: [ConversorComponent],
   providers: [MoedaService, ConversorService],
