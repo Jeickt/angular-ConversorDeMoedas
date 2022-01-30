b03d5dbac10490beca94c1fde8a882978bd173cc
Jeison Leandro Rückert <jeison.let@gmail.com>
2022-01-30 13:58:52 -0300

    Implementando o serviço de gerenciamento de moedas

--- src/app/conversor/conversor.module.ts
+++ src/app/conversor/conversor.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-
+import { ConversorComponent } from './components/conversor.component';
+import { MoedaService } from './services';
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule
-  ]
+  declarations: [ConversorComponent],
+  imports: [CommonModule],
+  exports: [ConversorComponent],
+  providers: [MoedaService],
 })
-export class ConversorModule { }
+export class ConversorModule {}
