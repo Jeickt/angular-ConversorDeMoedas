5046c83c7f66997fd1c0296416e57477b912d0de
Jeison Leandro Rückert <jeison.let@gmail.com>
2022-01-30 14:22:07 -0300

    Implementando o serviço de conversão de moedas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,12 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { ConversorModule } from './conversor';
-
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, ConversorModule],
+  imports: [BrowserModule, FormsModule, HttpClientModule, ConversorModule],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/app/conversor/conversor.module.ts
+++ src/app/conversor/conversor.module.ts
@@ -1,13 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
 
 import { ConversorComponent } from './components/conversor.component';
-import { MoedaService } from './services';
+import { MoedaService, ConversorService } from './services';
 
 @NgModule({
   declarations: [ConversorComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, HttpClientModule],
   exports: [ConversorComponent],
-  providers: [MoedaService],
+  providers: [MoedaService, ConversorService],
 })
 export class ConversorModule {}
